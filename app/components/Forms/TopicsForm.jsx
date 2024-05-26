db6771c2fbe26477256a1c650b98b809d961d529
Jovy Cortez <jovycortez11@gmail.com>
2024-05-25 20:52:25 -0400

    Modify Topics Model and API endpoint

---
+++ app/components/Forms/TopicsForm.jsx
@@ -0,0 +1,14 @@
+'use client'
+import React from 'react'
+
+const TopicsForm = () => {
+  return (
+    <div>
+        <div>
+            <input type="text" />
+        </div>
+    </div>
+  )
+}
+
+export default TopicsForm
