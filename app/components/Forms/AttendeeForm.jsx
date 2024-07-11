57b2d507723a40061414c577c003a2234a080f9c
Kaipher <kaipher@Kaiphers-MBP.attlocal.net>
2024-07-10 23:21:28 -0400

    Setup user selection of type complete

--- app/components/Forms/AttendeeForm.jsx
+++ app/components/Forms/AttendeeForm.jsx
@@ -3,42 +3,41 @@ import React, { useContext } from "react";
 import AttendeesContext from "@/context/AttendeesContext";
 
 const Form = () => {
-  const { handleChangeInput, formData, handleSubmit, isEditing } =
-    useContext(AttendeesContext);
-  const { id, firstName, lastName, email } = formData;
+  // const { handleChangeInput, formData, handleSubmit, isEditing } =    useContext(AttendeesContext);
+  // const { id, firstName, lastName, email } = formData;
   return (
-    <section className="form-section-4">
-      <div>{isEditing ? id : null}</div>
-      <form className="form-4" onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Jane"
-          className="form-input"
-          name="firstName"
-          onChange={handleChangeInput}
-          value={firstName}
-        />
-        <input
-          type="text"
-          placeholder="Doe"
-          className="form-input"
-          name="lastName"
-          onChange={handleChangeInput}
-          value={lastName}
-        />
-        <input
-          type="text"
-          placeholder="JaneDoe@mail.com"
-          className="form-input"
-          name="email"
-          onChange={handleChangeInput}
-          value={email}
-        />
-        <button className="button-68" onClick={handleSubmit}>
-          {isEditing ? "Save" : "Submit"}
-        </button>
-      </form>
-    </section>
+    // <section className="form-section-4">
+    //   <div>{isEditing ? id : null}</div>
+    //   <form className="form-4" onSubmit={handleSubmit}>
+    //     <input
+    //       type="text"
+    //       placeholder="Jane"
+    //       className="form-input"
+    //       name="firstName"
+    //       onChange={handleChangeInput}
+    //       value={firstName}
+    //     />
+    //     <input
+    //       type="text"
+    //       placeholder="Doe"
+    //       className="form-input"
+    //       name="lastName"
+    //       onChange={handleChangeInput}
+    //       value={lastName}
+    //     />
+    //     <input
+    //       type="text"
+    //       placeholder="JaneDoe@mail.com"
+    //       className="form-input"
+    //       name="email"
+    //       onChange={handleChangeInput}
+    //       value={email}
+    //     />
+    //     <button className="button-68" onClick={handleSubmit}>
+    //       {isEditing ? "Save" : "Submit"}
+    //     </button>
+    //   </form>
+    // </section>
   );
 };
 
--- app/components/Userprofile/Userprofile.jsx
+++ app/components/Userprofile/Userprofile.jsx
@@ -6,7 +6,11 @@ import { Button, Input, Typography } from '@mui/joy'
 const Userprofile = () => {
     const { users, setUsers } = useContext(UsersContext)
     const [userType, setUsersType] = useState({
-        firstname: "",
+        _id: users?.user?._id,
+        firstname: users?.user?.name,
+        email: users?.user?.email,
+        events: users?.user?.events,
+        topics: users?.user?.topics,
         lastname: "",
         title: "",
         phone: "",
@@ -28,48 +32,58 @@ const Userprofile = () => {
         }))
     }
     const handleAttendee = async () => {
-        
-
         setUsersType((user) => ({
             ...user,
+            // Assuming there might be additional user type modifications here
             role: "attendee"
-        }))
-    }
+        }));
+
+    };
+
+
     const handleSpeaker = async () => {
 
         setUsersType((user) => ({
             ...user,
+            // Assuming there might be additional user type modifications here
             role: "speaker"
-        }))
+        }));
+
+        
     }
     const handleSponsor = async () => {
 
         setUsersType((user) => ({
             ...user,
             role: "sponsor"
-        }))
+            // Assuming there might be additional user type modifications here
+        }));
+
+        
     }
     useEffect(() => {
-        console.log(userType)
-    }, [userType])
+      setUsersType(prev => ({
+        ...prev
+      }))
+    }, [])
     return (
         <div>
             Userprofile
-            {JSON.stringify(users?.user)}
+            {JSON.stringify(userType)}
             <Button onClick={handleAttendee} >Attendee</Button>
             <Button onClick={handleSpeaker} >Speaker</Button>
             <Button onClick={handleSponsor} >Sponsor</Button>
-            <Typography>{users?.user?.name}</Typography>
-            <Typography>{users?.user?.email}</Typography>
-            <Typography>{users?.user?.events}</Typography>
-            <Typography>{users?.user?.topics}</Typography>
-            <Input onChange={handleChange} value={userType.firstname} name='firstname' type='text' />
-            <Input onChange={handleChange} value={userType.lastname} name='lastname' type='text' />
-            <Input onChange={handleChange} value={userType.company} name='company' type='text' />
-            <Input onChange={handleChange} value={userType.phone} name='phone' type='text' />
-            <Input onChange={handleChange} value={userType.description} name='description' type='text' />
-            <Input onChange={handleChange} value={userType.presentation} name='presentation' type='text' />
-            <Input onChange={handleChange} value={userType.aboutme} name='aboutme' type='text' />
+            <Typography>{userType?.name}</Typography>
+            <Typography>{userType?.email}</Typography>
+            <Typography>{userType?.events}</Typography>
+            <Typography>{userType?.topics}</Typography>
+            <Input onChange={handleChange} value={userType?.firstname} name='firstname' type='text' />
+            <Input onChange={handleChange} value={userType?.lastname} name='lastname' type='text' />
+            <Input onChange={handleChange} value={userType?.company} name='company' type='text' />
+            <Input onChange={handleChange} value={userType?.phone} name='phone' type='text' />
+            <Input onChange={handleChange} value={userType?.description} name='description' type='text' />
+            <Input onChange={handleChange} value={userType?.presentation} name='presentation' type='text' />
+            <Input onChange={handleChange} value={userType?.aboutme} name='aboutme' type='text' />
 
         </div>
     )
